cd2fd780f56efadbf5cde91908dd912a6dccb7c2
Torben Grove <torben_grove@live.dk>
2018-04-17 13:49:25 +0200

    Gui hovedmenu videre udvikling
    Alignment opsætning og vinduesstørrelse

--- Program/KAS-Projekt/src/guifx/guiApp.java
+++ Program/KAS-Projekt/src/guifx/guiApp.java
@@ -2,6 +2,8 @@ package guifx;
 
 import application.service.Service;
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -25,12 +27,14 @@ public class guiApp extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		stage.setTitle("KAS System");
+		stage.setTitle("KAS");
 		BorderPane pane = new BorderPane();
 		this.initContent(pane);
 
 		Scene scene = new Scene(pane);
 		stage.setScene(scene);
+		stage.setMinHeight(160);
+		stage.setMinWidth(330);
 		stage.show();
 	}
 
@@ -44,19 +48,25 @@ public class guiApp extends Application {
 	}
 
 	private void initGridPane(GridPane gridPane) {
+		gridPane.setAlignment(Pos.CENTER);
+		gridPane.setHgap(5);
+		gridPane.setVgap(5);
 		Label lblWelcome = new Label("Velkommen til Konference Administrations Systemet");
 		gridPane.add(lblWelcome, 0, 0);
 
 		Button btnAddConference = new Button("Tilføj Konference");
 		btnAddConference.setMinWidth(150);
+		GridPane.setHalignment(btnAddConference, HPos.CENTER);
 		gridPane.add(btnAddConference, 0, 1);
 
 		Button btnAddOrganization = new Button("Tilføj Organisation");
 		btnAddOrganization.setMinWidth(150);
+		GridPane.setHalignment(btnAddOrganization, HPos.CENTER);
 		gridPane.add(btnAddOrganization, 0, 2);
 
 		Button btnAddParticipant = new Button("Tilføj Deltager");
 		btnAddParticipant.setMinWidth(150);
+		GridPane.setHalignment(btnAddParticipant, HPos.CENTER);
 		gridPane.add(btnAddParticipant, 0, 3);
 
 		btnAddConference.setOnAction(event -> this.addConference());
@@ -65,7 +75,7 @@ public class guiApp extends Application {
 
 	}
 
-	private void addParticipant() {
+	private void addConference() {
 		// TODO Auto-generated method stub
 		Alert alert = new Alert(AlertType.INFORMATION, "Her skal ske noget fedt!", ButtonType.CLOSE);
 		alert.setHeaderText("INFO");
@@ -79,7 +89,7 @@ public class guiApp extends Application {
 		alert.showAndWait();
 	}
 
-	private void addConference() {
+	private void addParticipant() {
 		// TODO Auto-generated method stub
 		Alert alert = new Alert(AlertType.INFORMATION, "Her skal ske noget fedt!", ButtonType.CLOSE);
 		alert.setHeaderText("INFO");
