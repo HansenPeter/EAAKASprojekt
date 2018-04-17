c9c9740109b9fcb31156c88165f0a7d05f2fc61d
Torben Grove <torben_grove@live.dk>
2018-04-17 11:12:09 +0200

    ja...

--- Program/KAS-Projekt/src/application/model/Tilmelding.java
+++ Program/KAS-Projekt/src/application/model/Tilmelding.java
@@ -1,5 +1,10 @@
 package application.model;
 
 public class Tilmelding {
+	private Deltager deltager;
 
+	public Tilmelding(Deltager deltager) {
+		// TODO Auto-generated constructor stub
+		this.deltager = deltager;
+	}
 }
