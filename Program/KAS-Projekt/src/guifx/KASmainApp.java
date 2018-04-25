38e08e74f6e14d946af8b0a9dad7dbc04e83f433
Torben Grove <torben_grove@live.dk>
2018-04-25 21:44:09 +0200

    Tweakin af hovedmenu, nu med KASkas

--- Program/KAS-Projekt/src/guifx/KASmainApp.java
+++ Program/KAS-Projekt/src/guifx/KASmainApp.java
@@ -2,16 +2,14 @@ package guifx;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -46,15 +44,23 @@ public class KASmainApp extends Application {
 
 	}
 
+	Label lblWelcome;
+	ListView lvwKonferences;
+	VBox vbButton;
+	GridPane gridButtons;
+	Button btnAddConference, btnAddParticipant, btnClose;
+
 	private void initGridPane(GridPane gridPane) {
 		gridPane.setAlignment(Pos.CENTER);
 		gridPane.setHgap(20);
 		gridPane.setVgap(10);
-		Label lblWelcome = new Label("Velkommen til Konference Administrations Systemet");
+		gridPane.setGridLinesVisible(false);
+		lblWelcome = new Label("Velkommen til Konference Administrations Systemet");
 		gridPane.add(lblWelcome, 0, 0);
 
-		ListView lvwKonferences = new ListView<String>();
-
+		lvwKonferences = new ListView<String>();
+		lvwKonferences.setMinHeight(100);
+		lvwKonferences.setMaxHeight(200);
 		gridPane.add(lvwKonferences, 0, 1, 1, 2);
 
 		// Midlertidig data til listview
@@ -62,27 +68,22 @@ public class KASmainApp extends Application {
 		lvwKonferences.getItems().add("Peter");
 		lvwKonferences.getItems().add("Made");
 
-		VBox vbButton = new VBox();
+		gridPane.add(new ImageView(GUITools.kasKas()), 1, 0, 1, 2);
 
-		gridPane.add(vbButton, 1, 1);
+		gridButtons = new GridPane();
+		gridButtons.setAlignment(Pos.CENTER);
+		gridButtons.setHgap(20);
+		gridButtons.setVgap(10);
+		gridPane.add(gridButtons, 1, 2);
+		btnAddConference = GUITools.stdButton("Tilføj Konference");
+		gridButtons.add(btnAddConference, 0, 0);
 
-		int addButtonWidth = 150;
-		int addButtonHeight = 100;
+		btnAddParticipant = GUITools.stdButton("Tilmeld Deltager");
+		gridButtons.add(btnAddParticipant, 0, 1);
 
-		Button btnAddConference = new Button("Tilføj Konference");
-		btnAddConference.setMinSize(addButtonWidth, addButtonHeight);
-		GridPane.setHalignment(btnAddConference, HPos.CENTER);
-		vbButton.getChildren().add(btnAddConference);
-
-		Button btnAddParticipant = new Button("Tilmeld Deltager");
-		btnAddParticipant.setMinSize(addButtonWidth, addButtonHeight);
-		GridPane.setHalignment(btnAddParticipant, HPos.CENTER);
-		vbButton.getChildren().add(btnAddParticipant);
-
-		Button btnClose = new Button("Luk Program");
-		btnClose.setMinWidth(150);
-		GridPane.setHalignment(btnClose, HPos.CENTER);
-		gridPane.add(btnClose, 1, 3);
+		btnClose = GUITools.stdButton("Luk Program");
+		GridPane.setValignment(btnClose, VPos.BOTTOM);
+		gridPane.add(btnClose, 1, 2);
 
 		btnAddConference.setOnAction(event -> this.addConference());
 		btnAddParticipant.setOnAction(event -> this.addParticipant());
@@ -98,13 +99,11 @@ public class KASmainApp extends Application {
 	}
 
 	private void addParticipant() {
-		
+
 		KASTilmeldDeltagerWindow window = new KASTilmeldDeltagerWindow();
 		window.showAndWait();
-		
-	}
 
-	
+	}
 
 	private void closeProgram() {
 		// TODO Auto-generated method stub
