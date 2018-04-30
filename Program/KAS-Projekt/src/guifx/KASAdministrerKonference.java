cd0761336397b173e903d99d527f992f6609aa32
Torben Grove <torben_grove@live.dk>
2018-04-30 12:20:53 +0200

    Administrer vindue up and running

--- Program/KAS-Projekt/src/guifx/KASAdministrerKonference.java
+++ Program/KAS-Projekt/src/guifx/KASAdministrerKonference.java
@@ -18,106 +18,134 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class KASAdministrerKonference extends Stage {
-    private Konference konference;
 
-    public KASAdministrerKonference(Konference konference) {
-        // TODO Auto-generated constructor stub
-        this.konference = konference;
-        setTitle("Administrer Konference");
-        GridPane gridPane = new GridPane();
-        initContent(gridPane);
+	private Konference konference;
+	Label lblBeboelser, lblUdflugter, lblNavn;
+	ListView<Beboelse> lvwBeboelser;
+	ListView<Udflugt> lvwUdflugter;
+	ListView<application.model.Service> lvwServices;
 
-        Scene scene = new Scene(gridPane);
-        setScene(scene);
+	Button btnAddBeboelse, btnAddServices, btnAddUdflugt, btnClose, btnShowLedsagere, btnShowDeltagere;
+	private ImageView KASKas;
 
-    }
+	public KASAdministrerKonference(Konference konference) {
+		// TODO Auto-generated constructor stub
+		this.konference = konference;
+		setTitle("Administrer Konference");
+		GridPane gridPane = new GridPane();
+		initContent(gridPane);
 
-    Label lblBeboelser, lblUdflugter, lblNavn;
-    ListView<Beboelse> lvwBeboelser;
-    ListView<Udflugt> lvwUdflugter;
-    ListView<application.model.Service> lvwServices;
+		Scene scene = new Scene(gridPane);
+		setScene(scene);
 
-    Button btnAddBeboelse, btnAddServices, btnAddUdflugt, btnClose;
-    private ImageView KASKas;
+	}
 
-    private void initContent(GridPane gridPane) {
-        // TODO Auto-generated method stub
-        gridPane.setPadding(new Insets(10));
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
+	private void initContent(GridPane gridPane) {
+		// TODO Auto-generated method stub
+		gridPane.setPadding(new Insets(10));
+		gridPane.setHgap(10);
+		gridPane.setVgap(10);
 
-        KASKas = new ImageView(GUITools.kasKas());
-        // Downscale hvis den er for stor
-        KASKas.setScaleX(2);
-        KASKas.setScaleY(2);
-        GridPane.setHalignment(KASKas, HPos.CENTER);
-        GridPane.setValignment(KASKas, VPos.CENTER);
-        gridPane.add(KASKas, 1, 0, 1, 2);
+		KASKas = new ImageView(GUITools.kasKas());
+		// Downscale hvis den er for stor
+		KASKas.setScaleX(2);
+		KASKas.setScaleY(2);
+		GridPane.setHalignment(KASKas, HPos.CENTER);
+		GridPane.setValignment(KASKas, VPos.CENTER);
+		gridPane.add(KASKas, 1, 0, 1, 2);
 
-        lblUdflugter = new Label("Udflugter");
-        gridPane.add(lblUdflugter, 0, 0);
+		lblUdflugter = new Label("Udflugter");
+		gridPane.add(lblUdflugter, 0, 0);
 
-        lvwUdflugter = new ListView<>();
-        lvwUdflugter.setMaxHeight(100);
-        lvwUdflugter.getItems().addAll(Service.getUdflugter(konference));
-        gridPane.add(lvwUdflugter, 0, 1);
+		lvwUdflugter = new ListView<>();
+		lvwUdflugter.setMaxHeight(100);
+		lvwUdflugter.getItems().addAll(Service.getUdflugter(konference));
+		gridPane.add(lvwUdflugter, 0, 1);
 
-        btnAddUdflugt = GUITools.stdButton("Tilfoej Udflugt");
-        gridPane.add(btnAddUdflugt, 0, 2);
-        btnAddUdflugt.setOnAction(event -> addUdflugtAction()); // MADE
+		btnAddUdflugt = GUITools.stdButton("Tilfoej Udflugt");
+		gridPane.add(btnAddUdflugt, 0, 2);
+		btnAddUdflugt.setOnAction(event -> addUdflugtAction()); // MADE
 
-        lblBeboelser = new Label("Beboelser");
-        gridPane.add(lblBeboelser, 0, 3);
+		btnShowLedsagere = GUITools.stdButton("Vis Ledsagere");
+		gridPane.add(btnShowLedsagere, 0, 3);
+		btnShowLedsagere.setOnAction(event -> showDeltagere());
 
-        lvwBeboelser = new ListView<>();
-        lvwBeboelser.setMaxHeight(100);
-        lvwBeboelser.getItems().addAll(Service.getBeboelser(konference));
-        gridPane.add(lvwBeboelser, 0, 4);
+		lblBeboelser = new Label("Beboelser");
+		gridPane.add(lblBeboelser, 0, 4);
 
-        btnAddBeboelse = GUITools.stdButton("Tilfoej Beboelse");
-        gridPane.add(btnAddBeboelse, 0, 5);
-        btnAddBeboelse.setOnAction(event -> addBeboelseAction()); // MADE
+		lvwBeboelser = new ListView<>();
+		lvwBeboelser.setMaxHeight(100);
+		lvwBeboelser.getItems().addAll(Service.getBeboelser(konference));
+		gridPane.add(lvwBeboelser, 0, 5);
 
-        lvwServices = new ListView<>();
-        lvwServices.setMaxHeight(100);
-        gridPane.add(lvwServices, 1, 4);
-        lvwBeboelser.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Beboelse>() {
-            @Override
-            public void changed(ObservableValue<? extends Beboelse> observable, Beboelse oldBeboelse,
-                    Beboelse newBeboelse) {
-                lvwServices.getItems().setAll(Service.getServices(lvwBeboelser.getSelectionModel().getSelectedItem()));
-            }
+		btnAddBeboelse = GUITools.stdButton("Tilfoej Beboelse");
+		gridPane.add(btnAddBeboelse, 0, 6);
+		btnAddBeboelse.setOnAction(event -> addBeboelseAction()); // MADE
 
-        });
+		btnShowDeltagere = GUITools.stdButton("Vis Deltagere");
+		gridPane.add(btnShowDeltagere, 0, 7);
+		btnShowDeltagere.setOnAction(event -> showLedsagere());
 
-        btnAddServices = GUITools.stdButton("Tilfoej Services");
-        gridPane.add(btnAddServices, 1, 5);
-        btnAddServices.setOnAction(event -> addServiceAction()); // MADE
+		lvwServices = new ListView<>();
+		lvwServices.setMaxHeight(100);
+		gridPane.add(lvwServices, 1, 5);
+		lvwBeboelser.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Beboelse>() {
+			@Override
+			public void changed(ObservableValue<? extends Beboelse> observable, Beboelse oldBeboelse,
+					Beboelse newBeboelse) {
+				lvwServices.getItems().setAll(Service.getServices(lvwBeboelser.getSelectionModel().getSelectedItem()));
+			}
 
-        btnClose = GUITools.stdButton("Close");
-        gridPane.add(btnClose, 1, 6);
-        GridPane.setHalignment(btnClose, HPos.RIGHT);
-        btnClose.setOnAction(event -> close());
+		});
 
-    }
+		btnAddServices = GUITools.stdButton("Tilfoej Services");
+		gridPane.add(btnAddServices, 1, 6);
+		btnAddServices.setOnAction(event -> addServiceAction()); // MADE
 
-    public void addUdflugtAction() {
-        KASTilfoejUdflugt udflugtWindow = new KASTilfoejUdflugt(this.konference);
-        udflugtWindow.showAndWait();
-        lvwUdflugter.getItems().setAll(Service.getUdflugter(konference));
+		btnClose = GUITools.stdButton("Close");
+		gridPane.add(btnClose, 1, 8);
+		GridPane.setHalignment(btnClose, HPos.RIGHT);
+		btnClose.setOnAction(event -> close());
 
-    }
+	}
 
-    public void addBeboelseAction() {
-        KASOpretBeboelse beboelseWindow = new KASOpretBeboelse(this.konference);
-        beboelseWindow.showAndWait();
-        lvwBeboelser.getItems().setAll(Service.getBeboelser(konference));
+	private void showDeltagere() {
+		// TODO Auto-generated method stub
+		if (lvwBeboelser.getSelectionModel().getSelectedItem() != null) {
+			KASDeltagerWindow window = new KASDeltagerWindow(konference,
+					lvwBeboelser.getSelectionModel().getSelectedItem());
+			window.showAndWait();
+		}
+		// else do nothing
+	}
 
-    }
+	private void showLedsagere() {
+		// TODO Auto-generated method stub
+		if (lvwUdflugter.getSelectionModel().getSelectedItem() != null) {
+			KASDeltagerWindow window = new KASDeltagerWindow(konference,
+					lvwUdflugter.getSelectionModel().getSelectedItem());
+			window.showAndWait();
+		}
+		// else do nothing
+	}
 
-    public void addServiceAction() {
-        KASTilfoejService serviceWindow = new KASTilfoejService(lvwBeboelser.getSelectionModel().getSelectedItem());
-        serviceWindow.showAndWait();
-        lvwServices.getItems().setAll(Service.getServices(lvwBeboelser.getSelectionModel().getSelectedItem()));
-    }
+	public void addUdflugtAction() {
+		KASTilfoejUdflugt udflugtWindow = new KASTilfoejUdflugt(this.konference);
+		udflugtWindow.showAndWait();
+		lvwUdflugter.getItems().setAll(Service.getUdflugter(konference));
+
+	}
+
+	public void addBeboelseAction() {
+		KASOpretBeboelse beboelseWindow = new KASOpretBeboelse(this.konference);
+		beboelseWindow.showAndWait();
+		lvwBeboelser.getItems().setAll(Service.getBeboelser(konference));
+
+	}
+
+	public void addServiceAction() {
+		KASTilfoejService serviceWindow = new KASTilfoejService(lvwBeboelser.getSelectionModel().getSelectedItem());
+		serviceWindow.showAndWait();
+		lvwServices.getItems().setAll(Service.getServices(lvwBeboelser.getSelectionModel().getSelectedItem()));
+	}
 }
