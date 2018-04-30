a3312374533c4b5c1fbb4f0c2e82dd8c0baf2441
Torben Grove <torben_grove@live.dk>
2018-04-30 10:46:09 +0200

    Tilmeldte deltagere kan hentes!

--- Program/KAS-Projekt/src/application/service/Service.java
+++ Program/KAS-Projekt/src/application/service/Service.java
@@ -15,172 +15,180 @@ import storage.Storage;
 
 public class Service {
 
-    public static Deltager createDeltager(String navn, String adresse, String by, String land, String tlfnr) {
-        Deltager deltager = new Deltager(navn, adresse, by, land, tlfnr);
-        Storage.addDeltager(deltager);
-        return deltager;
-    }
-    
-    public static ArrayList<Deltager> getDeltagere(){
-    	return Storage.getDeltagers();
-    }
+	public static Deltager createDeltager(String navn, String adresse, String by, String land, String tlfnr) {
+		Deltager deltager = new Deltager(navn, adresse, by, land, tlfnr);
+		Storage.addDeltager(deltager);
+		return deltager;
+	}
 
-    public static Deltager createDeltager(String navn, String firmanavn, String adresse, String by, String land,
-            String tlfnr, String firmatlfnr) {
-        Deltager deltager = new Deltager(navn, firmanavn, adresse, by, land, tlfnr, firmatlfnr);
-        Storage.addDeltager(deltager);
-        return deltager;
-    }
+	public static ArrayList<Deltager> getDeltagere() {
+		return Storage.getDeltagers();
+	}
 
-    public static void removeDeltager(Deltager deltager) {
-        for (Tilmelding tilmelding : deltager.getTilmeldinger()) {
-            tilmelding.getKonference().removeTilmelding(tilmelding);
-            removeLedsager(tilmelding);
-        }
-        Storage.removeDeltager(deltager);
-    }
+	public static Deltager createDeltager(String navn, String firmanavn, String adresse, String by, String land,
+			String tlfnr, String firmatlfnr) {
+		Deltager deltager = new Deltager(navn, firmanavn, adresse, by, land, tlfnr, firmatlfnr);
+		Storage.addDeltager(deltager);
+		return deltager;
+	}
 
-    public static Deltager getDeltager(String tlfnr) {
-        for (Deltager deltager : Storage.getDeltagers()) {
-            if (tlfnr.equals(deltager.getTlfnr())) {
-                return deltager;
-            }
-        }
-        return null;
-    }
+	public static void removeDeltager(Deltager deltager) {
+		for (Tilmelding tilmelding : deltager.getTilmeldinger()) {
+			tilmelding.getKonference().removeTilmelding(tilmelding);
+			removeLedsager(tilmelding);
+		}
+		Storage.removeDeltager(deltager);
+	}
 
-    public static void removeLedsager(Tilmelding tilmelding) {
-        Ledsager ledsager = tilmelding.getLedsager();
-        for (Udflugt udflugt : ledsager.getUdflugter()) {
-            udflugt.removeLedsager(ledsager);
-        }
-    }
+	public static Deltager getDeltager(String tlfnr) {
+		for (Deltager deltager : Storage.getDeltagers()) {
+			if (tlfnr.equals(deltager.getTlfnr())) {
+				return deltager;
+			}
+		}
+		return null;
+	}
 
-    // ---------------------------------------------------------------------------------------------------------------------------------------
+	public static ArrayList<Deltager> getDeltagere(Konference konference) {
+		ArrayList<Deltager> deltagere = new ArrayList<Deltager>();
+		for (Tilmelding tilmelding : konference.getTilmeldinger()) {
+			deltagere.add(tilmelding.getDeltager());
+		}
+		return deltagere;
+	}
 
-    public static Tilmelding createTilmelding(Deltager deltager, Konference konference, LocalDate ankomstdato,
-            LocalDate afrejsedato, boolean foredragsholder) {
-        Tilmelding tilmelding = deltager.createTilmelding(konference, ankomstdato, afrejsedato, foredragsholder);
-        deltager.addTilmelding(tilmelding);
-        return tilmelding;
-    }
+	public static void removeLedsager(Tilmelding tilmelding) {
+		Ledsager ledsager = tilmelding.getLedsager();
+		for (Udflugt udflugt : ledsager.getUdflugter()) {
+			udflugt.removeLedsager(ledsager);
+		}
+	}
 
-    // ---------------------------------------------------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------------------------------------
 
-    public static Booking createBooking(Tilmelding tilmelding, ArrayList<application.model.Service> services,
-            Beboelse beboelse) {
-        Booking booking = tilmelding.createBooking(services, beboelse);
-        return booking;
-    }
+	public static Tilmelding createTilmelding(Deltager deltager, Konference konference, LocalDate ankomstdato,
+			LocalDate afrejsedato, boolean foredragsholder) {
+		Tilmelding tilmelding = deltager.createTilmelding(konference, ankomstdato, afrejsedato, foredragsholder);
+		deltager.addTilmelding(tilmelding);
+		return tilmelding;
+	}
 
-    public static Booking createBooking(Tilmelding tilmelding, Beboelse beboelse) {
-        Booking booking = tilmelding.createBooking(beboelse);
-        return booking;
-    }
-    // ---------------------------------------------------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------------------------------------
 
-    public static Beboelse createBeboelse(String name, double prisEnkeltvaerelse, double prisDobbeltvaerelse) {
-        Beboelse beboelse = new Beboelse(name, prisEnkeltvaerelse, prisDobbeltvaerelse);
-        Storage.addBeboelse(beboelse);
-        return beboelse;
-    }
+	public static Booking createBooking(Tilmelding tilmelding, ArrayList<application.model.Service> services,
+			Beboelse beboelse) {
+		Booking booking = tilmelding.createBooking(services, beboelse);
+		return booking;
+	}
 
-    // ---------------------------------------------------------------------------------------------------------------------------------------
+	public static Booking createBooking(Tilmelding tilmelding, Beboelse beboelse) {
+		Booking booking = tilmelding.createBooking(beboelse);
+		return booking;
+	}
+	// ---------------------------------------------------------------------------------------------------------------------------------------
 
-    public static Ledsager createLedsager(Tilmelding tilmelding, String navn) {
-        Ledsager ledsager = tilmelding.createLedsager(navn);
-        return ledsager;
-    }
-    // ---------------------------------------------------------------------------------------------------------------------------------------
+	public static Beboelse createBeboelse(String name, double prisEnkeltvaerelse, double prisDobbeltvaerelse) {
+		Beboelse beboelse = new Beboelse(name, prisEnkeltvaerelse, prisDobbeltvaerelse);
+		Storage.addBeboelse(beboelse);
+		return beboelse;
+	}
 
-    public static Organisation createOrganisation(String navn) {
-        Organisation organisation = new Organisation(navn);
-        Storage.addOrganisation(organisation);
-        return organisation;
-    }
+	// ---------------------------------------------------------------------------------------------------------------------------------------
 
-    public static ArrayList<Organisation> getOrganisations() {
-        return new ArrayList<>(Storage.getOrganisations());
-    }
-    // ---------------------------------------------------------------------------------------------------------------------------------------
+	public static Ledsager createLedsager(Tilmelding tilmelding, String navn) {
+		Ledsager ledsager = tilmelding.createLedsager(navn);
+		return ledsager;
+	}
+	// ---------------------------------------------------------------------------------------------------------------------------------------
 
-    public static Konference createKonference(Organisation organisation, LocalDate startDato, LocalDate slutDato,
-            String lokation, String navn, String tema, double pris) {
-        Konference konference = organisation.createKonference(startDato, slutDato, lokation, navn, tema, pris);
-        Storage.addKonference(konference);
-        return konference;
-    }
+	public static Organisation createOrganisation(String navn) {
+		Organisation organisation = new Organisation(navn);
+		Storage.addOrganisation(organisation);
+		return organisation;
+	}
 
-    public static ArrayList<Konference> getSpecifikkeKonferencer(Organisation organisation) {
-        return Storage.getSpecifikkeKonferencer(organisation);
-    }
+	public static ArrayList<Organisation> getOrganisations() {
+		return new ArrayList<>(Storage.getOrganisations());
+	}
+	// ---------------------------------------------------------------------------------------------------------------------------------------
 
-    public static ArrayList<Konference> getKonferencer() {
-        return Storage.getKonferencer();
-    }
+	public static Konference createKonference(Organisation organisation, LocalDate startDato, LocalDate slutDato,
+			String lokation, String navn, String tema, double pris) {
+		Konference konference = organisation.createKonference(startDato, slutDato, lokation, navn, tema, pris);
+		Storage.addKonference(konference);
+		return konference;
+	}
 
-    public static LocalDate getKonferenceStartdato(Konference konference) {
-        return konference.getStartDato();
-    }
+	public static ArrayList<Konference> getSpecifikkeKonferencer(Organisation organisation) {
+		return Storage.getSpecifikkeKonferencer(organisation);
+	}
 
-    public static LocalDate getKonferenceSlutdato(Konference konference) {
-        return konference.getSlutDato();
-    }
+	public static ArrayList<Konference> getKonferencer() {
+		return Storage.getKonferencer();
+	}
 
-    public static Beboelse addBeboelseToKonference(Konference konference, String navn, double prisEnkelvaerelse,
-            double prisDobbeltvaerelse) {
-        Beboelse beboelse = new Beboelse(navn, prisEnkelvaerelse, prisDobbeltvaerelse);
-        konference.addBeboelse(beboelse);
-        Storage.addBeboelse(beboelse);
-        return beboelse;
-    }
-    // ---------------------------------------------------------------------------------------------------------------------------------------
+	public static LocalDate getKonferenceStartdato(Konference konference) {
+		return konference.getStartDato();
+	}
 
-    public static Udflugt createUdflugt(Konference konference, String navn, LocalDate dato, double pris,
-            boolean frokost) {
-        Udflugt udflugt = konference.createUdflugt(navn, dato, pris, frokost);
-        return udflugt;
-    }
+	public static LocalDate getKonferenceSlutdato(Konference konference) {
+		return konference.getSlutDato();
+	}
 
-    public static ArrayList<Udflugt> getUdflugter(Konference konference) {
-        return new ArrayList<>(konference.getUdflugter());
-    }
+	public static Beboelse addBeboelseToKonference(Konference konference, String navn, double prisEnkelvaerelse,
+			double prisDobbeltvaerelse) {
+		Beboelse beboelse = new Beboelse(navn, prisEnkelvaerelse, prisDobbeltvaerelse);
+		konference.addBeboelse(beboelse);
+		Storage.addBeboelse(beboelse);
+		return beboelse;
+	}
+	// ---------------------------------------------------------------------------------------------------------------------------------------
 
-    // --------------------------------------------------------------------------------------------------------------------
+	public static Udflugt createUdflugt(Konference konference, String navn, LocalDate dato, double pris,
+			boolean frokost) {
+		Udflugt udflugt = konference.createUdflugt(navn, dato, pris, frokost);
+		return udflugt;
+	}
 
-    public static double beregnSamletPris(Tilmelding tilmelding) {
-        return tilmelding.beregnSamletPris();
-    }
+	public static ArrayList<Udflugt> getUdflugter(Konference konference) {
+		return new ArrayList<>(konference.getUdflugter());
+	}
 
-    public static String getKonferenceNavn(Tilmelding tilmelding) {
-        return tilmelding.getKonference().getNavn();
-    }
+	// --------------------------------------------------------------------------------------------------------------------
 
-    public static void removeTilmelding(Tilmelding tilmelding) {
-        // TODO Auto-generated method stub
-        tilmelding.getDeltager().fjernTilmelding(tilmelding);
-    }
+	public static double beregnSamletPris(Tilmelding tilmelding) {
+		return tilmelding.beregnSamletPris();
+	}
 
-    public static ArrayList<Beboelse> getBeboelser(Konference konference) {
-        return new ArrayList<>(konference.getBeboelser());
-    }
+	public static String getKonferenceNavn(Tilmelding tilmelding) {
+		return tilmelding.getKonference().getNavn();
+	}
 
-    public static ArrayList<application.model.Service> getServices(Beboelse beboelse) {
-        return new ArrayList<>(beboelse.getServices());
-    }
+	public static void removeTilmelding(Tilmelding tilmelding) {
+		// TODO Auto-generated method stub
+		tilmelding.getDeltager().fjernTilmelding(tilmelding);
+	}
 
-    public static application.model.Service addService(Beboelse beboelse, String navn, String beskrivelse,
-            double pris) {
+	public static ArrayList<Beboelse> getBeboelser(Konference konference) {
+		return new ArrayList<>(konference.getBeboelser());
+	}
 
-        application.model.Service service = beboelse.createService(navn, beskrivelse, pris);
-        return service;
-    }
+	public static ArrayList<application.model.Service> getServices(Beboelse beboelse) {
+		return new ArrayList<>(beboelse.getServices());
+	}
 
-    public static Boolean hasLedsager(Tilmelding tilmelding) {
-        return tilmelding.getLedsager() != null;
-    }
+	public static application.model.Service addService(Beboelse beboelse, String navn, String beskrivelse,
+			double pris) {
 
-    public static Boolean hasOvernatning(Tilmelding tilmelding) {
-        return tilmelding.getBooking() != null;
-    }
+		application.model.Service service = beboelse.createService(navn, beskrivelse, pris);
+		return service;
+	}
+
+	public static Boolean hasLedsager(Tilmelding tilmelding) {
+		return tilmelding.getLedsager() != null;
+	}
+
+	public static Boolean hasOvernatning(Tilmelding tilmelding) {
+		return tilmelding.getBooking() != null;
+	}
 }
---
+++ Program/KAS-Projekt/src/guifx/KASDeltagerWindow.java
@@ -0,0 +1,54 @@
+package guifx;
+
+import application.model.Deltager;
+import application.model.Konference;
+import application.service.Service;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class KASDeltagerWindow extends Stage {
+	private Konference konference;
+	private Label lblDeltagere;
+	private ImageView kaskas = new ImageView(GUITools.kasKas());
+	private ListView<Deltager> lvwDeltagere;
+	private Button btnClose;
+
+	public KASDeltagerWindow(Konference konference) {
+		// TODO Auto-generated constructor stub
+		this.konference = konference;
+		this.setTitle("Deltagere");
+		GridPane gridPane = new GridPane();
+		this.initContent(gridPane);
+		Scene scene = new Scene(gridPane);
+		this.setScene(scene);
+
+	}
+
+	private void initContent(GridPane gridPane) {
+		// TODO Auto-generated method stub
+		gridPane.setPadding(new Insets(10));
+		gridPane.setHgap(10);
+		gridPane.setVgap(10);
+
+		gridPane.add(kaskas, 1, 0, 2, 1);
+		GridPane.setHalignment(kaskas, HPos.RIGHT);
+
+		lblDeltagere = new Label("Deltagere");
+		gridPane.add(lblDeltagere, 0, 0);
+
+		lvwDeltagere = new ListView<Deltager>();
+		lvwDeltagere.getItems().setAll(Service.getDeltagere(konference));
+		gridPane.add(lvwDeltagere, 0, 1);
+
+		btnClose = GUITools.stdButton("Close");
+		gridPane.add(btnClose, 1, 2);
+		btnClose.setOnAction(event -> this.close());
+	}
+}
